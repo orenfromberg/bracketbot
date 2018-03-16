72a1368aa87d6a0e30254f70e36a5ae1db3eafef
Oren Fromberg <orenfromberg@gmail.com>
2018-03-16 10:30:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+const Botkit = require("botkit");
+
+let controller = Botkit.slackbot({});
+let bot = controller.spawn();
+
+bot.configureIncomingWebhook({
+  url: process.env.WEBHOOK_URL
+});
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://adhoc.mayhem.cbssports.com");
+  await page.type("input#userid", process.env.USERNAME);
+  await page.type("input#password", process.env.PASSWORD);
+  await page.click('input[type="submit"]');
+  await page.waitForNavigation({
+    waitUntil: "domcontentloaded"
+  });
+  await page.goto("http://adhoc.mayhem.cbssports.com/brackets/standings");
+  const results = await page.evaluate(() => {
+    let text = "Ad Hoc Team United Standings\n";
+    text += `${document.querySelector("tr.footer").children[0].innerText}\n`;
+    text += "Rank\tTeam Name\n";
+    let foo = document.querySelector("table.data tbody");
+    for (let child of foo.children) {
+      if (child.id) {
+        let rank = child.children[0].innerText;
+        let name = child.children[1].innerText;
+        text += `${rank}\t${name}\n`;
+      }
+    }
+    return `\`\`\`${text}\`\`\``;
+  });
+
+  const msg = {
+    response_type: 'in_channel',
+    username: 'bracketbot',
+    attachments: [{
+      text: results,
+    }]
+  }
+
+  //console.log(results);
+  bot.sendWebhook(msg, (err, res) => {
+    if (err) throw err;
+
+    console.log(`\n🚀  Starbot report delivered 🚀`);
+  })
+  await browser.close();
+})();
